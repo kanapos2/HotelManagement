50a166135d94756f97742de78b56d9f133b26a34
kanapos <kanapas.l@ku.th>
2018-09-16 19:03:04 +0700

    update phone number

--- src/main/java/Admin/Manu_control.java
+++ src/main/java/Admin/Manu_control.java
@@ -1,7 +1,14 @@
 package Admin;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Manu_control {
 
@@ -9,7 +16,32 @@ public class Manu_control {
     private Label user ;
 
     @FXML
+    private Button btn_signout ;
+
+    @FXML
     public void setUser(String name) {
         user.setText(name);
+    }
+
+    @FXML
+    public void signOut(ActionEvent event) {
+        System.out.println("Sign out");
+
+        btn_signout = (Button) event.getSource();
+
+        Stage stage = (Stage) btn_signout.getScene().getWindow();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fontUI/Login_as_admin.fxml"));
+        try {
+            stage.setScene(new Scene(loader.load(), 1280, 720));
+            stage.setTitle("Login hotel management for admin");
+
+            Login_as_admin_control controller = (Login_as_admin_control) loader.getController();
+
+            stage.show();
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
     }
 }
