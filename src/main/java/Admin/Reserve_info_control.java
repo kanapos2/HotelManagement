76bbff0bb6f51ca51d5c75f61cffbbf80b712762
kanapos <kanapas.l@ku.th>
2018-10-13 22:20:23 +0700

    Update SE

--- src/main/java/Admin/Reserve_info_control.java
+++ src/main/java/Admin/Reserve_info_control.java
@@ -1,11 +1,11 @@
 package Admin;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,10 +15,11 @@ public class Reserve_info_control {
     private String nowLogin = "--";
 
     @FXML
-    private Label roomnumber , test;
+    private Label roomnumber , test , typeRoom;
 
     @FXML
-    private Button testback;
+    private Button confirm , cancel;
+
 
     @FXML
     public void roomNumber(String number) {
@@ -27,17 +28,42 @@ public class Reserve_info_control {
 
     @FXML
     public void name(String nameLogin){
-        test.setText(nameLogin);
+        test.setText("Login as : "+nameLogin);
         nowLogin = nameLogin;
         System.out.println(nowLogin);
     }
 
+    @FXML
+    public void showTypeRoom(String type){
+        typeRoom.setText(type);
+    }
+
+    @FXML
+    public void cancelReserve(ActionEvent event){
+        cancel = (Button) event.getSource();
+        Stage stage = (Stage) confirm.getScene().getWindow();
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fontUI/Manu.fxml"));
+
+        try {
+            stage.setScene(new Scene(loader.load(), 1280, 720));
+            stage.setTitle("Reserve hotel");
+
+            Manu_control controller = (Manu_control) loader.getController();
+            controller.setUserAfterClickRoom(nowLogin);
+
+            stage.show();
+
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
 
     @FXML
     public void back(ActionEvent event){
-        testback = (Button) event.getSource();
+        confirm = (Button) event.getSource();
 
-        Stage stage = (Stage) testback.getScene().getWindow();
+        Stage stage = (Stage) confirm.getScene().getWindow();
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fontUI/Manu.fxml"));
 
