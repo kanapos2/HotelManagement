a9c1fbbaa75f6cbf9480353195c45199147886da
b5810450733 <tanakrit.pho@ku.th>
2018-12-02 21:14:42 +0700

    onlineDB
    onlineDB

--- src/main/java/Store/DBConnector.java
+++ src/main/java/Store/DBConnector.java
@@ -6,23 +6,40 @@ public class DBConnector {
     Connection connect = null;
     static final private String dbURL = "jdbc:sqlite:hoteldb.db";
 
-
-    public Connection openDatabase() {
+    public Connection openMysql(){
         try {
-            Class.forName("org.sqlite.JDBC");
-            if(connect == null) {
-                connect = DriverManager.getConnection(dbURL);
+            Class.forName("com.mysql.jdbc.Driver");
+            // Setup the connection with the DB
+            if (connect == null){
+                connect = DriverManager.getConnection("jdbc:mysql://hoteladmin.c7r8xbm6ljha.ap-southeast-1.rds.amazonaws.com:3306/hoteladmin?"
+                        +"user=hoteladmin&password=#hoteladmin");
             }
-            if (connect != null) {
-                System.out.println("Connect to database!!!");
+            if (connect != null){
+                System.out.println("Connect to Mysql Complete.");
             }
-        } catch (ClassNotFoundException ex) {
-            ex.printStackTrace();
-        } catch (SQLException ex) {
-            ex.printStackTrace();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
         }
         return this.connect;
     }
+
+
+//    public Connection openDatabase() {
+//        try {
+//            Class.forName("org.sqlite.JDBC");
+//            if(connect == null) {
+//                connect = DriverManager.getConnection(dbURL);
+//            }
+//            if (connect != null) {
+//                System.out.println("Connect to database!!!");
+//            }
+//        } catch (ClassNotFoundException ex) {
+//            ex.printStackTrace();
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//        }
+//        return this.connect;
+//    }
 
     public void closeDatabase() {
         try {
@@ -52,14 +69,14 @@ public class DBConnector {
 
     public static RoomDBConnector openRoomDB(){
         DBConnector db = new DBConnector();
-        Connection connection = db.openDatabase();
+        Connection connection = db.openMysql();
         RoomDBConnector RoomControls = new RoomDBConnector(connection);
         return RoomControls;
     }
 
     public static UserDBControl openUserDB(){
         DBConnector db = new DBConnector();
-        Connection connection = db.openDatabase();
+        Connection connection = db.openMysql();
         UserDBControl UserControls = new UserDBControl(connection);
         return UserControls;
     }
--- src/main/java/Store/DBMain.java
+++ src/main/java/Store/DBMain.java
@@ -1,57 +1,57 @@
-//package Store;
-//
-//import Model.Room;
-//import Model.User;
-//import javafx.beans.property.SimpleIntegerProperty;
-//import javafx.beans.property.SimpleStringProperty;
-//
-//import java.sql.Connection;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//
-////public class DBMain {
-////    public static void main(String[] args) throws SQLException {
-////        RoomDBConnector f = DBConnector.openRoomDB();
-////        for (Room s: f.readHistory()) {
-////            System.out.println(s);
-////        }
-//
-//
-////        Room r1 = new Room("B211",0);
-////        System.out.println(rcb.addRoom(r1));
-//
-////        for (Room s : rcb.readRoom()){
-////            System.out.println(s);
-////        }
-////        ArrayList<Room> roomList = rcb.readRoom();
-////        for (Room i:roomList) {
-////            System.out.println(i);
-//////
-////        String c = "G7";
-////        for (int i=1 ; i<=11 ; i++) {
-////            DBConnector db = new DBConnector();
-////            Connection connection = db.openDatabase();
-////            UserDBControl userDBControl = new UserDBControl(connection);
-////            RoomDBConnector rcb = new RoomDBConnector(connection);
-////            if (i<10) {
-////                rcb.addRoom(new Room(c+"0"+i,0));
-////            }
-////            else {
-////                rcb.addRoom(new Room(c+i,0));
-////            }
+package Store;
+
+import Model.Room;
+import Model.User;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+//public class DBMain {
+//    public static void main(String[] args) throws SQLException {
+//        RoomDBConnector f = DBConnector.openRoomDB();
+//        for (Room s: f.readHistory()) {
+//            System.out.println(s);
+//        }
+
+
+//        Room r1 = new Room("B211",0);
+//        System.out.println(rcb.addRoom(r1));
+
+//        for (Room s : rcb.readRoom()){
+//            System.out.println(s);
+//        }
+//        ArrayList<Room> roomList = rcb.readRoom();
+//        for (Room i:roomList) {
+//            System.out.println(i);
 ////
-////        }
-////        }
+//        String c = "G7";
+//        for (int i=1 ; i<=11 ; i++) {
+//            DBConnector db = new DBConnector();
+//            Connection connection = db.openDatabase();
+//            UserDBControl userDBControl = new UserDBControl(connection);
+//            RoomDBConnector rcb = new RoomDBConnector(connection);
+//            if (i<10) {
+//                rcb.addRoom(new Room(c+"0"+i,0));
+//            }
+//            else {
+//                rcb.addRoom(new Room(c+i,0));
+//            }
 //
-////        User user = new User("000","admin","Admin",
-////                "Superadmin","8888888888","adminHR@hr.com","adminadmin");
-////        System.out.println(userDBControl.addUser(user));
-//
-//        /////////////////////////////////////////////////////// เอาไว้อ่านข้อมูลจาก database
-//
-////        ArrayList<User> userList = userDBControl.readUser();
-////        for (User i: userList) {
-////            System.out.println(i);
+//        }
+//        }
+
+//        User user = new User("000","admin","Admin",
+//                "Superadmin","8888888888","adminHR@hr.com","adminadmin");
+//        System.out.println(userDBControl.addUser(user));
+
+        /////////////////////////////////////////////////////// เอาไว้อ่านข้อมูลจาก database
+
+//        ArrayList<User> userList = userDBControl.readUser();
+//        for (User i: userList) {
+//            System.out.println(i);
 ////        }
 //    }
 //
